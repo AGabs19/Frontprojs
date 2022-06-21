61339dfc02cfe37c946950351ea6d574b47a969d
AGabs <anagabriella@infosiscs.com>
2022-06-21 15:36:28 -0300

    Finalizei o projeto, ficou uma gracinha

---
+++ WebDesigner/scripts/main.js
@@ -0,0 +1,30 @@
+(function(){
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+
+    // var $btnMenu = document.querySelector('.header__btnMenu')
+    // $btnMenu.removeAttribute('style')
+
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        widthEnabled: 1024 
+    })
+
+    var carouselImgs = new Caurosel({
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+    
+    var carouselQuotes = new CarouselDois({
+        container: '.quote-container .quote-slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+})()
